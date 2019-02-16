4b20fea9e173edd7b20e08f005b2f1ebe4fecedb
Humphryshikunzi <humphryshikunzi9@gmail.com>
2019-02-16 18:39:09 +0300

    All the files for common controls one and two

---
+++ UWPCommonControlsPart1/Commoncontrolspart2.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWPCommonControlsPart1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Commoncontrolspart2 : Page
+    {
+        public Commoncontrolspart2()
+        {
+            this.InitializeComponent();
+        }
+
+        private void CalendarView_OnSelectedDatesChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
+        {
+            var selecteddates = sender.SelectedDates
+                .Select(p => p.Date.Month.ToString() + " / " + p.Date.Day.ToString()).ToArray();
+            var values = string.Join(" , ", selecteddates);
+            CalenderViewTextBlock.Text = values;
+        }
+
+        private void MyInnerFlyoutButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            MyFlyout.Hide();
+           // throw new NotImplementedException();
+        }
+
+        private List<string> Names = new List<string>()
+        {
+            "Humphry", "Alfred", "Alex", "Allan", "Brednda", "Carson", "Frankline", "Randy"
+        };
+
+        private void AutoSuggestBox_OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            var autosuggestionbox = (AutoSuggestBox)sender;
+            var filtered = Names.Where(p => p.StartsWith(autosuggestionbox.Text)).ToArray();
+            autosuggestionbox.ItemsSource = filtered;
+
+        }
+    }
+}
---
+++ UWPCommonControlsPart1/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPCommonControlsPart1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void CheckBox_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            TextBlock4.Text = CheckBox.IsChecked.ToString();
+        }
+
+        private void RadioButton1_OnChecked(object sender, RoutedEventArgs e)
+        {
+            TextBlock5.Text =  RadioButton1.IsChecked.ToString();
+        }
+
+        private void ComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var combo = (ComboBox) sender;
+            var item = (ComboBoxItem)combo.SelectedItem;
+            TextBlock1.Text = item.Content.ToString();
+        }
+
+       
+
+        private void ToggleButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            TextBlock6.Text = ToggleButton.IsChecked.ToString();
+        }
+
+      /*  private void MyListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selecteditems = MyListBox.Items.Cast<ListBoxItem>()
+                .Where(t => t.IsSelected).Select(p => p.Content.ToString()).ToArray();
+            TextBlock3.Text = String.Join(" , ", selecteditems.ToString());
+
+            
+        }*/
+      private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+      {
+          Frame.Navigate(typeof(Commoncontrolspart2));
+      }
+    }
+}
